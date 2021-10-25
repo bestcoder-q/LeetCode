af1b4cd0721c2aecf88bc87bad129307bc84fb60
bestcoder-q <441217467@qq.com>
2021-10-25 10:01:47 +0800

    删除test.java

---
+++ src/tjpu/edu/binarytree/InorderTraversal.java
@@ -0,0 +1,7 @@
+package tjpu.edu.binarytree;
+
+/**
+ * Created by Lwq on 2021/10/25 9:44
+ */
+public class InorderTraversal {
+}
--- src/tjpu/edu/binarytree/test.java
+++
@@ -1,7 +0,0 @@
-package tjpu.edu.binarytree;
-
-/**
- * Created by Lwq on 2021/4/28 23:25
- */
-public class test {
-}
