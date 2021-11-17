d2f6dc73c81f3b91433e73c6a6576a2ec17fe552
bestcoder-q <441217467@qq.com>
2021-11-17 09:59:29 +0800

    344-反转字符串

---
+++ src/tjpu/edu/manipulatestr/AddStrings.java
@@ -0,0 +1,12 @@
+package tjpu.edu.manipulatestr;
+
+/**
+ * Created by Lwq on 2021/11/16 20:42
+ * https://leetcode-cn.com/problems/add-strings/
+ * 415. 字符串相加
+ */
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        return null;
+    }
+}
---
+++ src/tjpu/edu/manipulatestr/ReverseString.java
@@ -0,0 +1,29 @@
+package tjpu.edu.manipulatestr;
+
+/**
+ * Created by Lwq on 2021/11/16 23:43
+ * https://leetcode-cn.com/problems/reverse-string/
+ * 344. 反转字符串
+ */
+public class ReverseString {
+    public void reverse(char[] s) {
+        int i = 0, j = s.length - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            ++i;
+            --j;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = new char[]{'h','e','l','l','o'};
+        ReverseString reverseString = new ReverseString();
+        reverseString.reverse(s);
+        for (char c : s) {
+            System.out.println(c);
+        }
+    }
+    
+}
