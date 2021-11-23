63a2d9ba500266e1259a18e9d85eaf05a3e55fad
bestcoder-q <441217467@qq.com>
2021-11-23 10:32:03 +0800

    258-各位相加

---
+++ src/tjpu/edu/manipulatenum/AddDigits.java
@@ -0,0 +1,46 @@
+package tjpu.edu.manipulatenum;
+
+/**
+ * Created by Lwq on 2021/11/22 22:10
+ * https://leetcode-cn.com/problems/add-digits/
+ * 258. 各位相加
+ */
+public class AddDigits {
+    /*
+    方法1：双层while循环
+     */
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int now = 0;
+            while (num != 0) {
+                now = now + (num % 10);
+                num = num / 10;
+            }
+            num = now;
+        }
+        return num;
+    }
+    
+    /*
+    方法2：递归
+     */
+//    public int addDigits(int num) {
+//        int sum = 0;
+//        while (num != 0) {
+//            sum = sum + (num % 10);
+//            num = num /10;
+//        }
+//        if (sum % 10 == sum) {
+//            return sum;
+//        } else {
+//            return addDigits(sum);
+//        }
+//    }
+    
+
+    public static void main(String[] args) {
+        AddDigits ad = new AddDigits();
+        System.out.println(ad.addDigits(632));
+    }
+    
+}
