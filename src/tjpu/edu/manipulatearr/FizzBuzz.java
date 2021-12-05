965759df15907bbdeda6612f9264f431c6f807ae
bestcoder-q <441217467@qq.com>
2021-12-05 23:09:27 +0800

    412-FizzBuzz

---
+++ src/tjpu/edu/manipulatearr/FizzBuzz.java
@@ -0,0 +1,37 @@
+package tjpu.edu.manipulatearr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lwq on 2021/12/5 22:06
+ * https://leetcode-cn.com/problems/fizz-buzz/
+ * 412.FizzBuzz
+ */
+public class FizzBuzz {
+    public List<String> writeFizzBuzz(int n) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 1; i <= n; ++i) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(i + "");
+            }
+        }
+        return list;
+    }
+
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        List<String> res = fizzBuzz.writeFizzBuzz(15);
+        for (String re : res) {
+            System.out.println(re);
+        }
+    }
+    
+}
---
+++ src/tjpu/edu/manipulatearr/MissingNumber.java
@@ -0,0 +1,36 @@
+package tjpu.edu.manipulatearr;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Lwq on 2021/11/23 22:06
+ * https://leetcode-cn.com/problems/missing-number/
+ * 268.丢失的数字
+ */
+public class MissingNumber {
+    public int findMissingNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        //初始化map
+        for (int i = 0; i <= nums.length; ++i) {
+            map.put(i, 0);
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> integerEntry : map.entrySet()) {
+            if (map.get(integerEntry.getKey()) == 0) {
+                return integerEntry.getKey();
+            }
+        }
+        return -1;
+    }
+
+    
+    public static void main(String[] args) {
+        int[] nums = new int[]{0};
+        MissingNumber missingNumber = new MissingNumber();
+        System.out.println(missingNumber.findMissingNumber(nums));
+    }
+    
+}
